fa9faa7e0108faff424498078f8ed9ce859b9a95
Aleksandr Koloshtivin <aykolosh@mts.ru>
2022-02-02 11:10:43 +0300

    Добавил spin loop

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,24 @@ public class Main {
     public static void main(String[] args) {
         final CarShop shop = new CarShop();
 
-        new Thread(new Customer(shop)).start();
-        new Thread(new Customer(shop)).start();
-        new Thread(new Customer(shop)).start();
+        Thread customer1 = new Thread(new Customer(shop));
+        customer1.setName("Покупатель 1");
+        Thread customer2 = new Thread(new Customer(shop));
+        customer2.setName("Покупатель 2");
+        Thread customer3 = new Thread(new Customer(shop));
+        customer3.setName("Покупатель 3");
 
-
+        customer1.start();
+        customer2.start();
+        customer3.start();
         new Thread(null, shop::recieveCar, "Производитель").start();
+
+        while(shop.store.getStoreSize() != 10) {
+            Thread.onSpinWait();
+        }
+
+        customer1.interrupt();
+        customer2.interrupt();
+        customer3.interrupt();
     }
 }
