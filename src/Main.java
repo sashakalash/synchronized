07144288de404be524d58942cff0a280f7f5d3a5
Aleksandr Koloshtivin <aykolosh@mts.ru>
2022-02-04 21:55:08 +0300

    Создание бесконечных поток покупателей и произволителя

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,31 @@
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
+    private static final int CAR_MANUFACTURING_TIME = 3;
+    private static final int CUSTOMER_ARRIVING_TIME = 1;
+    private static final int THREAD_DELAY = 0;
 
     public static void main(String[] args) {
         final CarShop shop = new CarShop();
 
-        new Thread(null, shop::sellCar, "Покупатель 1").start();
-        new Thread(null, shop::sellCar, "Покупатель 2").start();
-        new Thread(null,  shop::sellCar, "Покупатель 3").start();
-        new Thread(null, shop::recieveCar, "Производитель").start();
+        ThreadGroup mainGroup = new ThreadGroup("main group");
 
-        while(shop.store.size() != 10) {
+        makeScheduleExecutor(new Thread(mainGroup, shop::sellCar, "Покупатель 1"), CUSTOMER_ARRIVING_TIME);
+        makeScheduleExecutor(new Thread(mainGroup, shop::sellCar, "Покупатель 2"), CUSTOMER_ARRIVING_TIME);
+        makeScheduleExecutor(new Thread(mainGroup, shop::sellCar, "Покупатель 3"), CUSTOMER_ARRIVING_TIME);
+        makeScheduleExecutor(new Thread(mainGroup, shop::recieveCar, "Производитель"), CAR_MANUFACTURING_TIME);
+
+        while(shop.soldCars < 10) {
             Thread.onSpinWait();
         }
+        mainGroup.interrupt();
+    }
+
+    public static void makeScheduleExecutor(Runnable thread, int period) {
+        ScheduledExecutorService executor =
+                Executors.newSingleThreadScheduledExecutor();
+        executor.scheduleAtFixedRate(thread, THREAD_DELAY, period, TimeUnit.SECONDS);
     }
 }
--- src/Seller.java
+++ src/Seller.java
@@ -1,7 +1,5 @@
 public class Seller {
     private CarShop carShop;
-    private static final int CAR_SELLING_TIME = 1000;
-    private static final int CAR_MANUFACTORING_TIME = 1000;
     private static final Car NEW_CAR = new Car("Toyota");
 
     public Seller(CarShop carShop) {
@@ -9,32 +7,23 @@ public class Seller {
     }
 
     public synchronized void receiveCar() {
-        try {
-            System.out.println("Продавец ожидает поставку");
-            Thread.sleep(CAR_MANUFACTORING_TIME);
-            System.out.println("Производитель Toyota выпустил 1 авто");
+        while (carShop.store.size() == 0) {
+            System.out.println("Производитель выпустил 1 авто");
             carShop.store.add(NEW_CAR);
-            notify();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            notifyAll();
         }
     }
 
-    public synchronized Car sellCar() {
-        System.out.printf("%s зашел в автосалон\n", Thread.currentThread().getName());
+    public synchronized void sellCar() {
         try {
             while (carShop.store.size() == 0) {
-                Thread.sleep(CAR_SELLING_TIME);
                 System.out.println("Продавец: Машин нет в наличии в вашей комплектации");
                 wait();
             }
-            Thread.sleep(CAR_SELLING_TIME);
-            System.out.println("Продавец: На складе появился автомобиль");
-            Thread.sleep(CAR_SELLING_TIME);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         System.out.printf("%s уехал на новеньком авто\n", Thread.currentThread().getName());
-        return carShop.store.remove(0);
+        carShop.store.remove(0);
     }
 }
